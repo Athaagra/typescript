411cce32c32084bedf63a85b8d20ed3e0f9a7a9f
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 05:54:54 +0200

    Create strboolnum.ts

---
+++ strboolnum.ts
@@ -0,0 +1,19 @@
+/** String Types **/
+var firstname: string = "John";
+firstname = 1;
+let automobile = "BMW";
+const city = "New York";
+let students = 32;
+let studentAsString: string = students.toString();
+
+/** Number types **/
+var age: number = 32;
+let year: number =2024;
+const numberOfMembers = 61;
+numberOfMembers = 128;
+let stringToNumber: number = parseInt("1985");
+
+/** Booleans **/
+let isStudent: boolean = false;
+const alwaysStudent = true;
+let minimumAge: boolean = age > 6 ? true : false;
