23b52063b80b922f41998b9e4f531720894498f3
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 06:22:25 +0200

    Create symbol.ts

---
+++ symbol.ts
@@ -0,0 +1,14 @@
+let id: symbol = Symbol(1234);
+let alphabetical: symbol = Symbol("id");
+
+let user = {
+  [id]: "1234",
+  name: "Mark",
+  getId(){
+    return this[id];
+  },
+};
+
+console.log(user.name);
+console.log(id);
+console.log(user.getId);
