10548714d3b9604624b6f4636f8342d83604ef25
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 15:16:24 +0200

    Create object.ts

---
+++ object.ts
@@ -0,0 +1,18 @@
+let car: Object = {
+  brand: "BMW",
+  color: "Black",
+};
+
+// not needed
+car = [];
+car = () => {};
+
+let newCar:{
+  brand: string;
+  color: string;
+} = {
+  brand: "BMW",
+  color: "Black",
+};
+
+newCar = [];
