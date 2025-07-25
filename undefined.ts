306fa114d6f26c801496c467dcb83392bcfcf306
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 06:02:13 +0200

    Create undefined.ts

---
+++ undefined.ts
@@ -0,0 +1,14 @@
+let user: undefined;
+
+let userRole: null;
+userRole = null;
+
+console.log(userRole = user);
+
+if(!userRole){
+  console.log("This condition is true");
+}
+
+if(!user){
+  console.log("This condition is true");
+}
