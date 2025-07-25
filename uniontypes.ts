2c542ec43e6051b39506bb19d46a807b5badddd9
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 07:45:09 +0200

    Create uniontypes.ts

---
+++ uniontypes.ts
@@ -0,0 +1,16 @@
+type StringOrNumber = string | number;
+type NumberOrUndefined = number | undefined;
+type StringNumberOrUndefined = string | number | undefined;
+type DateOrUndefined = Date | undefined;
+
+let stringOrNumber: StringOrNumber = 1233;
+
+function print(input: string | undefined){
+  if(input){
+    console.log(input);
+  }
+  console.log("Please input something to print");
+}
+
+print();
+print("Hello World");
