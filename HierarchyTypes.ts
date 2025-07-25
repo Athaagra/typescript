35d166c303b643b16ab1273526d7022d5948d085
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 08:08:53 +0200

    Create HierarchyTypes.ts

---
+++ HierarchyTypes.ts
@@ -0,0 +1,16 @@
+// A subtype will always exten the parent type 
+type check = any extends unknown ? true : false;
+type check2 = string extends any ? true : false;
+type check3 = number extends any ? true : false;
+type check4 = {} extends Object ? true : false;
+type check5 = [] extends Object ? true : false;
+type check6 = Function extends Object ? true : false;
+type check7 = never extends any ? true : false;
+
+// A function declaration that never completes and returns a never type
+const throwError = (errorMsg: string) => {
+  throw new Error(errorMsg);
+};
+
+let strings: Object = ["a","b"];
+let myFunc: Function = () => 2;
