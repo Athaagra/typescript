4f34409694dd3c1b2077ff079c7e2aa40ff788ab
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 07:27:54 +0200

    Create customtype.ts

---
+++ customtype.ts
@@ -0,0 +1,9 @@
+type CustomString = string;
+type CustomNumber = number;
+type CustomDate = Date;
+type CustomSymbol = Symbol;
+
+let firstName: CustomString = "Mark";
+let age: CustomNumber = 32;
+let today: CustomDate = new Date();
+let unique: CustomSymbol = Symbol();
