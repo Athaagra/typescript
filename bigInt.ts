05981d94ed0e6d4e4edfc07569d563fd95ea1e32
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-25 06:14:14 +0200

    Create bigInt.ts

---
+++ bigInt.ts
@@ -0,0 +1,16 @@
+const safeInt = Number.MAX_SAFE_INTEGER;
+console.log(safeInt);
+const safeIntPlusOne = safeInt + 1;
+const safeIntPlusTwo = safeInt + 2;
+console.log(safeIntPlusOne);
+console.log(safeIntPlusTwo);
+
+let bigInt1: bigint = BigInt(1234);
+let bigInt2: bigint = 123434543;
+console.log(bigInt1);
+
+let c: bigint = bigInt1 - bigInt2;
+
+let e:bigint = 12345.6n;
+
+let f = Math.log(bigInt1);
