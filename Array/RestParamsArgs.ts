520520a1b1c84545e1feefff0ad0f60c8ef09bb7
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-26 10:19:00 +0200

    Create RestParamsArgs.ts

---
+++ Array/RestParamsArgs.ts
@@ -0,0 +1,15 @@
+function multiplyBy(by: number, ...numbers: number[]){
+  return numbers.map((number) => by * number);
+}
+
+console.log(multiplyBy(2,3,4,5,6));
+console.log(multiplyBy(2,3,4,5));
+
+const args = [8,5];
+const angle = Math.atan2(...args);
+
+const args1 = [8,5] as const;
+const angle1 = Math.atan2(...args1);
+
+const args2: [number,number] = [8,5];
+const angle2 = Math.ata2(...args2);
