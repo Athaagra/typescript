f205863c5004b644c7a4760728d9ea919e0c59eb
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-26 18:35:49 +0200

    Create GenericFunctionDeclaration.ts

---
+++ generic/GenericFunctionDeclaration.ts
@@ -0,0 +1,23 @@
+type GetFirstElement = <T>(arr: T[])=>T;
+
+const getFirstElement: GetFirstElement = (arr) =>{
+  return arr[0];
+}
+
+const numbersArray = [1, 23, 4, 5];
+const stingArray = ["a","v"];
+
+let numberOutput = getFirstElement(numbersArray);
+let stringOutput = getFirstElement<string>(stringArray);
+
+type FirstElement<T> = (arr: T[]) => T;
+
+const firstElement: FirstElement<string> = (arr) => {
+  return arr[0];
+};
+
+const firstElementNumber: FirstElement<number> = (arr) => {
+  return arr[0];
+};
+
+firstElement(["123"]);
