2521770f719af7e2f1fddeb63b6bd24ad617bdfd
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-26 04:46:22 +0200

    Create EnumUnion.ts

---
+++ Array/EnumUnion.ts
@@ -0,0 +1,27 @@
+enum ShapeKind {
+  Circle = "circle",
+  Square = "square",
+}
+
+type Circle = {
+  kind: ShapeKind.Circle;
+  radius: number;
+};
+
+type Square = {
+  kind: ShapeKind.Square;
+  sideLength: number;
+};
+
+let circle:Circle ={
+  radius: 100,
+  kind: ShapeKind.Square,
+};
+
+function printShape(shape: ShapeKind /** Circle | Square */){
+  console.log(shape);
+}
+
+printShape(ShapeKind)
+
+
