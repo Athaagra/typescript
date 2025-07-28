ba7bcb34cd54aa57bf92389ffdc60a64e8c37bfe
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-28 23:09:32 +0200

    Create interface.ts

---
+++ interface/interface.ts
@@ -0,0 +1,33 @@
+interface User{
+  userName:string;
+  email: string;
+  login(): void;
+}
+
+class Admin implements User{
+  constructor(public username: string, public email: string, public adminLevel: number){}
+
+  login():void{
+    console.log("admin is now logged in");
+  }
+}
+
+class Customer implements User {
+  constructor(public userName: string, public email: string){}
+
+  login():void{
+    console.log("Admin is now logged in");
+  }
+}
+
+class Auth {
+  public static login(user:User){
+    user.login();
+  }
+}
+
+const admin: Admin = new Admin("mark","mark@email.com",1);
+const customer: Customer = new Customer("John","john@email.com");
+
+Auth.login(admin);
+Auth.login(customer);
