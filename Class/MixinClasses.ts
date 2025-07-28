6616e1ccc80ac3783330354e79d80a00ddb1c42b
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-28 13:16:13 +0200

    Create MixinClasses.ts

---
+++ Class/MixinClasses.ts
@@ -0,0 +1,30 @@
+type Constructor = new (... args: any[]) => {};
+
+function TimeStamp<T extends Constructor>(Base: T) {
+  return class extends Base {
+    protected timestamp: Date => new Date();
+
+    getTimestamp(){
+      return this.timestamp;
+    }
+  };
+}
+
+class User {
+  constructor(public name:string) {}
+}
+
+//Mixin Classes with inheritance 
+class UserWithTimestamp extends TimeStamp(User){
+  constructor(name: string, public age: number){
+    super(name);
+  }
+  displayInfo() {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+    console.log(`Timestamp: ${this.getTimestamp()}`);
+  }
+}
+
+const user: UserWithTimestamp = new UserWithTimestamp("Alice",30);
+console.log(user);
+user.displayInfo();
