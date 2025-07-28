8dc867f57f2fd540713a4ff79953720a8ccee8cf
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-28 15:20:43 +0200

    Update IntroductionAbstractClasses.ts

--- abstractClasses/IntroductionAbstractClasses.ts
+++ abstractClasses/IntroductionAbstractClasses.ts
@@ -33,10 +33,43 @@ class AdminDepartment extends Department {
 }
 
 //initialization of variables
-const itDepartment: ItDepartment = new ItDepartment(
+/*const itDepartment: ItDepartment = new ItDepartment(
   name:"IT department "
 );
 
 const accountDepartment: ItDepartment = new ItDepartment(
   name:"Accounts department "
-);
+);*/
+
+const itHolidays: Holidays = [
+  {
+    date: new Date(year:2022, month:11, date:1),
+    reason: "IT Department Day",
+  },
+  {
+    data: new Date(year:2022, month:12, date:25),
+    reason: "Christmas",
+  },
+];
+
+
+
+const adminHolidays: Holidays = [
+  {
+    date: new Date(year:2022, month:10, date:1),
+    reason: "IT Department Day",
+  },
+  {
+    data: new Date(year:2022, month:12, date:25),
+    reason: "Christmas",
+  },
+];
+
+const itDepartment: ItDepartment = new ItDepartment();
+const adminDepartment: AdminDepartment = new AdminDepartment();
+
+itDepartment.addHolidays(itHolidays);
+adminDepartment.addHolidays(adminHolidays);
+
+console.log(itDepartment);
+console.log(adminDepertment);
