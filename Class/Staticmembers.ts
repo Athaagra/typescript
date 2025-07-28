64fc9fd03e816167a5df9f397b83ae1e542b53b2
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-28 11:19:00 +0200

    Create Staticmembers.ts

---
+++ Class/Staticmembers.ts
@@ -0,0 +1,14 @@
+class Counter {
+  static count= 0;
+
+  static increment(){
+    Counter.count++;
+  }
+}
+
+console.log(Counter.count);
+Counter.increment();
+console.log(Counter.count);
+
+const counter: Counter = new Counter();
+counter.count;
