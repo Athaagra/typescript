d08bc38644c31f7722544f9600b69e26302a74ae
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-28 12:16:07 +0200

    Create UseCaseGenericStatus.ts

---
+++ Class/UseCaseGenericStatus.ts
@@ -0,0 +1,38 @@
+type Identifiable = {
+  id: number;
+};
+
+class Repository<T extends Identifiable>{
+  private items: T[] = [];
+
+  add(item: T){
+    this.items.push(item);
+  }
+
+  getByid(id: number): any | undefined {
+    return this.items.find((item) => item.id === id);
+  }
+
+  getAll(): T[] {
+    return this.items;
+  }
+
+  removebyId(id: number): void{
+    this.items = this.items.filter((item) => item.id === item.id);
+  }
+}
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+const usersRepository = new Repository<User>();
+usersRepository.add({
+  id:1,
+  name: "John",
+  email: "john@email.com"
+});
+
+console.log(usersRepository.getByid(1));
