bda3f7a35784b9212e2f3c4c51e84dcaab8d838e
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2025-07-29 22:52:33 +0200

    Create main1_prototype_function.js

---
+++ recap/main1_prototype_function.js
@@ -0,0 +1,60 @@
+class User {
+  constructor(name,email){
+    this.name = name;
+    this.email = email;
+    this.point = 0;
+  }
+  login(){
+    console.log(this.name, 'Has logged in');
+  }
+  logoout(){
+    console.log(this.name, 'Has logged out');
+  }
+  addPoint(){
+    this.points++;
+    console.log('total points',this.points);
+    
+  }
+}
+
+
+function User(name,email){
+  this.name = name;
+  this.email = email;
+  this.point = 0;
+
+  this.login = () => {
+    console.log(this.name, 'Has logged in');
+  };
+  this.logout = () => {
+    console.log(this.name, 'Has logged out');
+  };
+
+  this.addPoint = () => {
+    this.points++;
+    console.log('total points', this.points);
+  };
+  
+}
+
+function User(name,email){
+  this.name = name;
+  this.email = email;
+  this.point = 0;
+}
+
+User.prototype.login = function(){
+  console.log(this.name, 'Has logged in');
+};
+User.prototype.logout = function(){
+  console.log(this.name, 'Has logged out');
+};
+User.prototype.addPoint = function(){
+  this.points++;
+  console.log('total points',this.points);
+};
+const user = new User('John','john@email.com');
+user.addPoint();
+user.prototype.addPoint();
+
+console.log(user);
