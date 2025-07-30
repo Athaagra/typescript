ae44f2e95f884aeb1aa64c1b5535643304219550
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-07-30 22:05:23 +0200

    Update firstDecorator.ts

--- decorator/firstDecorator.ts
+++ decorator/firstDecorator.ts
@@ -1,1 +1,35 @@
+// Creating a Decorator
+function DoSomething(){
+    // do something with 'target'...
+}
+
+//Using a Decorator
+@DoSomething
+class User{
+  // members of the class
+}
+
+//How a decorator can be used property
+class User{
+  @DoSomething
+  name: string;
+}
+
+//How a decorator can be used in method
+class User{
+  @DoSomething
+  signIn(){}
+}
+
+//How a decorator can be used in properties
+class User {
+  constructor(
+    @DoSomething public name: string
+  ){}
+}
+//mutators
+class User{
+  @DoSomething
+  public get fullname(){}
+}
 
