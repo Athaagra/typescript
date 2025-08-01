de2154a3bb8159b7bea3cf03ddb1707c9578a66a
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-01 22:59:15 +0200

    Create utilitytypes.ts

---
+++ Utility/utilitytypes.ts
@@ -0,0 +1,7 @@
+//Utility types are predefined generic types that can perform common type transformation in TypeScript
+These types are built into TypeScript and are used to manipulate other types and create new types
+
+-Partial
+-Readonly
+-Pick
+-Exclude
