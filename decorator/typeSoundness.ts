d688a61ca8a47b8e3436838ba03d9fd6c9336eb1
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-01 20:37:08 +0200

    Create typeSoundness.ts

---
+++ decorator/typeSoundness.ts
@@ -0,0 +1,46 @@
+let value: unknown = "Hello, Typescript!";
+
+let str: number = value  as number;
+
+Type User = {
+  name: string;
+  age: number;
+};
+
+const user = {name:"Alice",age:30,isAdmin: true};
+const newUser: User = user;
+
+function printUser(user: User){
+  console.log(user);
+}
+
+printUser(newUser);
+printUser({name:"Alice",age:30,isAdmin:true});
+
+type Animal = {
+  name: string;
+};
+
+type Dog = Animal & {
+  breed: string;
+};
+
+let handleAnimal = (animal: Animal) => {
+  console.log(`Handling ${animal.name}`);
+};
+
+let handleDog: (dog:Dog) => void = handleAnimal;
+handleDog({name: "Buddy", breed:"Labrador"});
+
+function logNumbers(...numbers:number[]){
+  console.log(numbers);
+}
+
+logNumbers();
+
+function runFunction(func: ()=> void){
+  func();
+}
+const getPI = () => 3.14;
+
+runFunction(getPI);
