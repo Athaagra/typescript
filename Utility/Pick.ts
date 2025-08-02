9d0004de65b57d0129e2a38ed708b21a21d9d5f5
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-02 08:51:14 +0200

    Create Pick.ts

---
+++ Utility/Pick.ts
@@ -0,0 +1,13 @@
+// Pick<Type, Keys>
+interface Person {
+  name:string;
+  age: number;
+  address: string;
+}
+
+type NameAndAge = Pick<Person, "name" | "age" >;
+
+const person: NameAndAge = {
+  name: "John",
+  age: 32,
+};
