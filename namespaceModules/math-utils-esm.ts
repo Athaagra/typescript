6971febf17a26a0c1d3682b733bb8a35648aeea4
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-02 17:00:59 +0200

    Create math-utils-esm.ts

---
+++ namespaceModules/math-utils-esm.ts
@@ -0,0 +1,11 @@
+import { CommonUtils } from "./common-utils-esm";
+
+namespace MathUtils {
+  export function add(a:number,b:number){
+    CommonUtils.log(`Adding ${a} and ${b}`);
+    return a+b;
+  }
+  eport function substract(a:number, b:number){
+    return a-b;
+  }
+}
