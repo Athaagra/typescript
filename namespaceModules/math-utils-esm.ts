c8d584add922050e82f73d5f668ff5e3ce1c0cde
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-02 17:07:58 +0200

    Update math-utils-esm.ts

--- namespaceModules/math-utils-esm.ts
+++ namespaceModules/math-utils-esm.ts
@@ -1,9 +1,10 @@
+import AddOutput from "./AddOutput-interface.js"
 import { CommonUtils } from "./common-utils-esm.js";
 
 export class MathUtils {
-  export function add(a:number,b:number){
+  export function add(a:number,b:number):AddOutput{
     CommonUtils.log(`Adding ${a} and ${b}`);
-    return a+b;
+    return {sum:a+b,class:"MathUtils",};
   }
   eport function substract(a:number, b:number){
     return a-b;
