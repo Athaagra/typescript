dfb9bb4a8b64da413d28326573ff36f7d10ab671
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-02 16:27:12 +0200

    Create namespace.ts

---
+++ namespaceModules/namespace.ts
@@ -0,0 +1,20 @@
+namespace MathUtils {
+  export function add(a:number, b:number){
+    return a + b;
+  }
+  export function subtract(a:number, b:number){
+    return a - b; 
+  }
+}
+
+namespace StringUtils {
+  export function add(a:string, b:string){
+    return a + b;
+  }
+  export function subtract(a: string, b:string){
+    return a.replace(b,"");
+  }
+}
+
+let sum = MathUtils.add(5,2);
+let combined = StringUtils.add("Hello","World");
