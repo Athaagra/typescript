26a0148489ad87e20935ce79ed0b00dd316de9fb
Kel3vra <46803277+Athaagra@users.noreply.github.com>
2025-08-02 16:35:06 +0200

    Create app.ts

---
+++ namespaceModules/app.ts
@@ -0,0 +1,8 @@
+////<reference path="utils/math-utils.ts" />
+////<reference path="utils/string-utils.ts" />
+
+let sum = MathUtils.add(5,2);
+let combined = StringUtils.add("Hello ","World");
+
+console.log(sum);
+console.log(combined);
